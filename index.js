98d04b90f0d74a46a9d58961061677c2ca535796
luistei17 <luisedteixeira@gmail.com>
2019-05-19 09:27:14 -0300

    feat: google search for images

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const robots = {
     'userInput': require('./robots/input.js'),
     'text': require('./robots/text.js'),
-    'file': require('./robots/file')
+    'file': require('./robots/file.js'),
+    'image': require('./robots/image.js')
 };
 
 async function start () {
-    robots.userInput();
-    await robots.text();
+//    robots.userInput();
+//    await robots.text();
+    await robots.image();
 
     const savedContent = robots.file.load();
-
-    console.dir(savedContent, { depth: null });
 }
 
 start();
