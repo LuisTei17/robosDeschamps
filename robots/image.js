e61c67842df32d69a6acf9a5d019a8f58c9803b5
luistei17 <luisedteixeira@gmail.com>
2019-05-21 00:58:28 -0300

    feat: image downloading

--- robots/image.js
+++ robots/image.js
@@ -1,7 +1,8 @@
 const file = require('./file'),
     google = require('googleapis').google,
     customSearch = google.customsearch('v1'),
-    googleApiCred = require('./../credentials/google-search.json');
+    googleApiCred = require('./../credentials/google-search.json'),
+    imageDownloader = require('image-downloader');
 
 async function robots () {
     const content = file.load();
@@ -13,7 +14,7 @@ async function robots () {
         sentence.googleSearchQuery = query;
     }
 
-    console.dir(content, {depth: null});
+    await downloadAllImages(content);
 
     async function fetchImageUrls (query) {
         const response = await customSearch.cse.list({
@@ -26,6 +27,39 @@ async function robots () {
         if (response.data && response.data.items)
             return response.data.items.map((item) => item.link);
         return [];
+    }
+
+    async function downloadAllImages (content) {
+        const imagesDownloaded = [];
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images;
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                try {
+                    const imageUrl = images[imageIndex],
+                        imageAlreadyDownloaded = imagesDownloaded.includes(imageUrl);
+
+                    if (imageAlreadyDownloaded)
+                        throw new Error('Image already downloaded');
+
+                    await downloadAndSaveImage(imageUrl, `${sentenceIndex}-image.png`);
+
+                    imagesDownloaded.push(imageUrl);
+                    console.log(`image: ${imageUrl} downloaded successfully!!!`);
+                    break;
+                } catch (error) {
+                    console.log(`ERROR: ${error}`);
+                }
+            }
+        }
+    }
+
+    async function downloadAndSaveImage (url, _fileName) {
+        return imageDownloader.image({
+            url: url,
+            dest: `./content/${_fileName}`
+        });
     }
 }
 
